542744125f6c43c7755978ef89389b42e749be67
Kaizer Volfrang <60936706+KaizerVolfrang@users.noreply.github.com>
2021-02-13 12:36:40 +0300

    Add files via upload

---
+++ View/DisciplinesWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace View
+{
+    /// <summary>
+    /// Interaction logic for DisciplinesWindow.xaml
+    /// </summary>
+    public partial class DisciplinesWindow : Window
+    {
+        private JournalStorage _context;
+
+        public DisciplinesWindow(JournalStorage context)
+        {
+            InitializeComponent();
+            _context = context;
+        }
+        
+        //public void ShowDisciplines()
+        //{
+        //    var disciplines = _context.Disciplines.ToList();
+        //    disciplinesBox.ItemsSource = disciplines;
+        //    disciplinesBox.SelectedItem = disciplines[0];
+        //}
+
+    }
+}
---
+++ View/GroupsWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Model;
+
+namespace View
+{
+    /// <summary>
+    /// Interaction logic for GroupsWindow.xaml
+    /// </summary>
+    public partial class GroupsWindow : Window
+    {
+        private JournalStorage _context;
+        public GroupsWindow(JournalStorage context)
+        {
+            InitializeComponent();
+            _context = context;
+
+            var groups = _context.Groups.ToList();
+            groupComboBox.ItemsSource = groups;
+            groupComboBox.SelectedItem = groups[0];
+        }
+
+        private void openButton_Click(object sender, RoutedEventArgs e)
+        {
+            var groups = _context.Groups.ToList();
+            groupComboBox.ItemsSource = groups;
+            groupComboBox.SelectedItem = groups[0];
+            var disciplinesWindow = new DisciplinesWindow(_context);
+            Hide();
+            disciplinesWindow.ShowDialog();
+        }
+        
+    }
+}
